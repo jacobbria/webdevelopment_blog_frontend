e886996fe84233125a64267174896d1ef1a690a6
Jacob Bria <138321940+jacobbria@users.noreply.github.com>
2025-04-10 17:55:48 -0400

    Created error handling for user signs ups

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -7,17 +7,41 @@
         <h2>Create an account to get started</h2>
         <hr>
         <div class="input-section ">
+          <h2>Name</h2>
+          <input v-model="nameInput" type="text" id="nameInput" name="nameInput" placeholder="Enter your name" class="label">
+        </div>
+        <div class="error-container d-flex justify-content-center">
+          <div class="error-section" id="name-error">
+            <p>Must be between 5 and 30 characters</p>
+          </div>
+        </div>
+        <div class="input-section" >
           <h2>Email</h2>
           <input v-model="email" type="text" id="email" name="email" placeholder="Enter your email" class="label">
+        </div>
+        <div class="error-container d-flex justify-content-center">
+          <div class="error-section " id="email-error">
+            <p>Requires @franklin.edu & valid characters</p>
+          </div>
         </div>
         <div  class="input-section">
           <h2> Password</h2>
           <input v-model="password" type="password" id="password" name="password" placeholder="Enter your password" class="label">
         </div>
+        <div class="error-container d-flex justify-content-center">
+          <div class="error-section " id="password-error">
+            <p>Must be between 8 and 20 characters</p>
+          </div>
+        </div>
         <div  class="input-section">
           <h2>Re-enter Password</h2>
-          <input type="password" id="passwordRentry" name="password" placeholder="Re-enter your password" class="label">
+          <input type="password" id="passwordRentry" name="passwordRentry" placeholder="Re-enter your password" class="label">
          </div>   
+         <div class="error-container d-flex justify-content-center">
+          <div class="error-section " id="reenterpassword-error">
+            <p>Passwords must match</p>
+          </div>
+        </div>
         <button class="btn btn-primary mt-3" @click="signup">Sign up</button>
         </div>
       </div>
@@ -27,39 +51,113 @@
 
 <script setup>
 // Imports
+import { useRouter } from 'vue-router';
 import Navbar from '../components/Nav/Navbar.vue';
 import TheFooter from '../components/Nav/TheFooter.vue';
 import { ref } from "vue";
 
 // Define reactive variables for form fields
+const nameInput = ref("");
 const email = ref("");
 const password = ref("");
+const passwordRentry = ref("");
+
+const router = useRouter();
 
 // Login function to send a POST request
 const signup = async () => { 
-  console.log(email.value + " " + password.value);
+  console.log(nameInput.value + " " + email.value + " " + password.value);
   try {
-    const response = await fetch("api-goes here", { // TO DO -- input API || response is JS object holding server meta data
+    const response = await fetch("http://localhost:8081/api/create", { // TO DO -- input API || response is JS object holding server meta data
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        name: nameInput.value,
         email: email.value, 
         password: password.value,
       }),
     });
 
-    if (!response.ok) {
-      console.log("Error -  failed");
+
+     console.log(password.value + " " + passwordRentry.value);
+    if (password.value !== passwordRentry.value) {
+      let element =  document.getElementById("reenterpassword-error");
+      element.style.visibility = "visible";
+      password.value = ""
+      passwordRentry.value = "";
+      console.log("passwords dont match")
       return;
     }
+
     const result = await response.json();
-    console.log(result);
+    
+    if (!response.ok) {
+      console.log("Error - failed");
+      console.log("Status Code:", result.statusCode);
+      console.log("Error Message:", result.statusMsg);
+      displayError(result);
+      return;
+    } else {
+      console.log("Success!");
+      router.push({ name: 'Home' });
+    }
   } catch (error) {
     console.error("Error logging in:", error);
   }
 };
+
+const displayError = (result) => {
+  let element = null;
+    if (result.statusMsg.includes("name")){
+      element = document.getElementById("name-error");
+      element.style.visibility = "visible";
+
+      nameInput.value = "";
+      element = document.getElementById("nameInput");
+      element.style.outline = "2px solid red";
+    } else {
+      element = document.getElementById("name-error");
+      element.style.visibility = "hidden";
+      element = document.getElementById("nameInput");
+      element.style.outline = "none";
+    }
+
+    if (result.statusMsg.includes("email")){
+      element = document.getElementById("email-error");
+      element.style.visibility = "visible";
+
+      email.value = "";
+      element = document.getElementById("email");
+      element.style.outline = "2px solid red";
+    } else {
+      element = document.getElementById("email-error");
+      element.style.visibility = "hidden";
+      element = document.getElementById("email");
+      element.style.outline = "none";
+    }
+
+    if (result.statusMsg.includes("password")){
+      element = document.getElementById("password-error");
+      element.style.visibility = "visible";
+
+      password.value = "";
+      element = document.getElementById("password");
+      element.style.outline = "2px solid red";
+      element = document.getElementById("passwordRentry");
+      element.style.outline = "2px solid red";
+    } else {
+      element = document.getElementById("password-error");
+      element.style.visibility = "hidden";
+      
+      element = document.getElementById("password");
+      element.style.outline = "none";
+      element = document.getElementById("passwordRentry");
+      element.style.outline = "none";
+    }
+
+}
 </script>
 
 <style scoped>
@@ -85,6 +183,7 @@ hr {
   margin-bottom: 1rem;
   align-content: start;
   justify-content: center;
+  z-index: 10;
 }
 .input-section h2 {
   text-align: start;
@@ -93,5 +192,20 @@ iframe {
   pointer-events: none; /* Prevent iframe from being clickable but allows above elements to be clickable*/
   background: transparent !important;
   aspect-ratio: 1 / 1;
+}
+.error-section {
+  visibility: hidden;
+  background-color:rgb(255, 94, 94);
+  margin-top: -20px;
+  z-index: 1;
+  width: 95%;
+  height: 30px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+
+}
+.error-section p {
+  font-size: small;
 }
 </style>
