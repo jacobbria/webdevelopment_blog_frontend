378aabbcae5d6ea32108fc4554ee0ccbbe95ad93
Jacob Bria <138321940+jacobbria@users.noreply.github.com>
2025-05-29 21:03:21 -0400

    Added image assets for later integration.

--- src/views/account_Views/AccountSettings.vue
+++ src/views/account_Views/AccountSettings.vue
@@ -1,14 +1,20 @@
 <template>
-    <div>
-        <h2>Name</h2>
-        <h2>Password</h2>
-        <h2>Email</h2>
-        <h2>Phone</h2>
-        <h2>User Icon</h2>
-    </div>
+    <Navbar />
+        <CenterModalLayout>
+            <h2>Name</h2>
+            <h2>Password</h2>
+            <h2>Email</h2>
+            <h2>Phone</h2>
+            <h2>User Icon</h2>
+        </CenterModalLayout>
+    <TheFooter />
 </template>
 
 <script setup>
+import CenterModalLayout from '../../components/Templates/CenterModalLayout.vue';
+import Navbar from '../../components/Nav/Navbar.vue';
+import TheFooter from '../../components/Nav/TheFooter.vue';
+
 
 </script>
 
